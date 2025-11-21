2c8bdd1d890baea6b680e923ab4c4b97e50b7387
christiepang <christiepang2017@gmail.com>
2025-11-21 09:19:54 +0800

    Update 21_11_2025.py

--- 21_11_2025.py
+++ 21_11_2025.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Business Dashboard with Streamlit Layouts")
-msg="## Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard."
+msg="Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard."
 st.write(msg)
 
 col1, col2, col3 = st.columns(3)
