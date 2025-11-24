3c8dc4cda4f93f99876921fcfc44d0584779a240
christiepang <christiepang2017@gmail.com>
2025-11-24 14:32:56 +0800

    Update madedata.py

--- madedata.py
+++ madedata.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import streamlit as st
 
+data={'Product':['A','B','C'],
+      'Sales':[1200,850,950],
+      'Customers':[300,400,350]}
+
+df = pd.DataFrame(data)
+
+st.dataframe(df)
+st.data_editor(df)
+st.table(df)
+
+st.dataframe(df.style.format({'Sales':'$$${:,.0f}','Customers':'Number = {:,.0f}'}))
